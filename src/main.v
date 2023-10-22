08610c11e0c673ff406cfedddd9c1138de2e59f5
Nick Hay <nickjhay@gmail.com>
2023-10-22 00:57:00 -0700

    More cells, with names

--- src/main.v
+++ src/main.v
@@ -15,8 +15,29 @@ module tt_um_nickjhay_processor (
 
 	wire [7:0] sys_out[0:7];
 
-	systolic_cell(
+	systolic_cell s00 (
 		ui_in[0], sys_out[0][0], clk
+	);
+	systolic_cell s01 (
+		ui_in[1], sys_out[0][1], clk
+	);
+	systolic_cell s02 (
+		ui_in[2], sys_out[0][2], clk
+	);
+	systolic_cell s03 (
+		ui_in[3], sys_out[0][3], clk
+	);
+	systolic_cell s04 (
+		ui_in[4], sys_out[0][4], clk
+	);
+	systolic_cell s05 (
+		ui_in[5], sys_out[0][5], clk
+	);
+	systolic_cell s06 (
+		ui_in[6], sys_out[0][6], clk
+	);
+	systolic_cell s07 (
+		ui_in[7], sys_out[0][7], clk
 	);
 
 	//for (int j=0; j<8; j++) 
